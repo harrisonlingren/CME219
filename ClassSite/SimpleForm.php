24e4b6c4e15269832a39f2a0004b4ef0560599e6
Harrison Lingren <harrisonlingren@gmail.com>
2015-12-02 15:00:34 -0500

    changes from class

---
+++ ClassSite/SimpleForm.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<meta http-equiv="refresh" content="2; url=http://blue.butler.edu/~hlingren/CME219/ClassSite/pizzaForm.html">
+	</head>
+	<body>
+		<h1>Thank you!</h1>
+		Redirecting...
+	</body>
+</html>
